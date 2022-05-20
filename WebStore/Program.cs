a8d0fc2320e9e917abdc852c7af201d7cea0eb94
DenZone1 <dissap95-d@yandex.ru>
2022-05-20 16:15:00 +0400

    Первый контроллер

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,10 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 //конфигурирование состаных частей приложения
+
+builder.Services.AddControllersWithViews();
+
+
 var app = builder.Build();
 
 //var greeting = app.Configuration["ServerGreeting"];
 //app.MapGet("/", () => greeting);
 
-app.MapGet("/", () => app.Configuration["ServerGreeting"]);
+app.MapGet("/greetings", () => app.Configuration["ServerGreeting"]);
+
+app.MapDefaultControllerRoute();   
+
 
 app.Run();
