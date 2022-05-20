cefe16dfe130fe2dd8b773d6273a005d56d11f6f
DenZone1 <dissap95-d@yandex.ru>
2022-05-20 15:53:41 +0400

    Работа с файлом конфигурации

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -2,7 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 //конфигурирование состаных частей приложения
 var app = builder.Build();
 
-var greeting = app.Configuration["ServerGreeting"];
-app.MapGet("/", () => greeting);
+//var greeting = app.Configuration["ServerGreeting"];
+//app.MapGet("/", () => greeting);
+
+app.MapGet("/", () => app.Configuration["ServerGreeting"]);
 
 app.Run();
