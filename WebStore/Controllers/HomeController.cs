bdd9506409846379be77e011dda5cfa12920daec
DenZone1 <dissap95-d@yandex.ru>
2022-05-24 15:42:03 +0400

    Контроллер сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -6,37 +6,17 @@ namespace WebStore.Controllers;
 public class HomeController : Controller
 {
 
-    private static readonly List<Employee> __Employees = new()
-    {
-        new Employee {Id = 1, LastName = "Ivanov", Name ="Petr", Patronymic="Vasyl`evich", Age="57" },
-        new Employee {Id = 2, LastName = "Petrov", Name ="Ivan", Patronymic="Sydorovisch", Age="18" },
-        new Employee {Id = 3, LastName = "Pilat", Name ="Pontyi", Patronymic="Rimskii", Age="85" },
-    };
 
 
-    public IActionResult Index()
-    {
-        return View();
-        //return Content("Index");
-    }
+    public IActionResult Index() => View();
+
 
     public IActionResult Greetings(string? id)
     {
         return Content($"Controller Alive - {id}");
     }
 
-    public IActionResult Emloyees()
-    {
-        return View(__Employees);
-    }
-    public IActionResult EmployeeDetails(int Id)
-    {
-        var employee = __Employees.FirstOrDefault(x => x.Id == Id);
-        if (employee is null)
-        return NotFound();
-            
-        return View(employee);
-    }
+   
 
     public IActionResult Contacts() => View();
 
