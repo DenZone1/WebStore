07193d4caf6409e2d5f04f533856633bef26d194
DenZone1 <dissap95-d@yandex.ru>
2022-05-26 15:46:34 +0400

    Интерфейс сервиса сотрудника

---
+++ WebStore/Sevices/Interfaces/IEmpoyeesData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WebStore.Models;
+
+namespace WebStore.Sevices.Interfaces;
+
+public interface IEmpoyeesData
+{
+    IEnumerable<Employee> Getall();
+
+    Employee GetById(int id);
+
+    int Add(Employee employee);
+
+    bool Edit(Employee employee);
+
+    bool Delete(int Id);
+}
+
