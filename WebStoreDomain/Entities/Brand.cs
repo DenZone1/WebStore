4b61d479f918fc6e12db5614dde3d3a8cbca9777
DenZone1 <dissap95-d@yandex.ru>
2022-05-29 19:59:11 +0400

    Понятие бренда и Секции(категории)

---
+++ WebStoreDomain/Entities/Brand.cs
@@ -0,0 +1,18 @@
+﻿using WebStoreDomain.Entities.Base;
+using WebStoreDomain.Entities.Base.Interfaces;
+
+namespace WebStoreDomain.Entities;
+
+public class Brand : NamedEntity, IOrderedEntity
+{
+    public int Order { get; set; }
+}
+
+public class Section : NamedEntity, IOrderedEntity
+{
+    public int Order { get; set; }
+
+    public int? ParrentId { get; set; }
+}
+
+
---
+++ WebStoreDomain/Entities/Section.cs
@@ -0,0 +1,13 @@
+﻿using WebStoreDomain.Entities.Base;
+using WebStoreDomain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities;
+
+
+public class Section : NamedEntity, IOrderedEntity
+{
+    public int Order { get; set; }
+
+    public int? ParrentId { get; set; }
+}
+
