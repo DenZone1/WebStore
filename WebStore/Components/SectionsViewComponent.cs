b53fac5ff118cc18522772685b7c3bb6c301a7ce
DenZone1 <dissap95-d@yandex.ru>
2022-05-31 20:49:06 +0400

    Логика Визуального компонента категорий

--- WebStore/Components/SectionsViewComponent.cs
+++ WebStore/Components/SectionsViewComponent.cs
@@ -2,9 +2,42 @@
 
 using Microsoft.AspNetCore.Mvc;
 
+using WebStore.Sevices.Interfaces;
+using WebStore.ViewModels;
+
 namespace WebStore.Components;
 
 public class SectionsViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke() => View();
+    private readonly IProductData _ProductData;
+
+    public SectionsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
+    public IViewComponentResult Invoke()
+    {
+        var sections = _ProductData.GetSections();
+
+        var parent_sections = sections.Where(s => s.ParentId is null).OrderBy(s=>s.Order);
+
+
+        var parent_sections_views = parent_sections.
+            Select(s => new SectionViewModel 
+            { 
+                    Id = s.Id,
+                    Name = s.Name,
+            });
+
+        foreach (var parent_section in parent_sections_views)
+        {
+            var childs = sections.Where(s => s.ParentId == parent_section.Id);
+            foreach (var child_section in childs.OrderBy(s => s.Order))
+                parent_section.ChildSections.Add(new()
+                {
+                    Id = child_section.Id,
+                    Name= child_section.Name,
+                }); 
+        }
+
+        return View(parent_sections_views);
+    }
 }
