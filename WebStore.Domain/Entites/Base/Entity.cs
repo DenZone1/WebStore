a29992212c692a62fa8990859e742ba17d70526b
DenZone1 <dissap95-d@yandex.ru>
2022-05-31 19:48:09 +0400

    Базовые классы сущностей

---
+++ WebStore.Domain/Entites/Base/Entity.cs
@@ -0,0 +1,31 @@
+﻿using WebStore.Domain.Entites.Base.Interfaces;
+
+namespace WebStore.Domain.Entites.Base;
+
+public abstract class Entity : IEntity, IEquatable<Entity>
+{
+    public int Id { get; set; }
+
+    public bool Equals(Entity? other)
+    {
+       if(other is null) return false;
+       if(ReferenceEquals(this, other)) return true;
+       return Id == other.Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((Entity)obj);
+    }
+
+    public override int GetHashCode() => Id;
+
+    public static bool operator ==(Entity? left, Entity? right) => Equals(left, right);
+    public static bool operator !=(Entity? left, Entity? right) => !Equals(left, right);
+
+
+}
+
--- WebStore.Domain/Entites/Base/Interfaces/IOrderedEntity.cs
+++ WebStore.Domain/Entites/Base/Interfaces/IOrderedEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebStore.Domain.Entites.Base.Interfaces;
+﻿namespace WebStore.Domain.Entites.Base.Interfaces;
 
 public interface IOrderedEntity : IEntity
 {
