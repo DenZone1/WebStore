9eddb3db092168ce30c2b285f44b9198e5591a37
DenZone1 <dissap95-d@yandex.ru>
2022-05-31 20:26:14 +0400

    ЗАготовка дял сервиса католога товаров

---
+++ WebStore/Sevices/InMemoryProductData.cs
@@ -0,0 +1,16 @@
+﻿
+using WebStore.Data;
+using WebStore.Domain.Entites;
+using WebStore.Sevices.Interfaces;
+
+namespace WebStore.Sevices;
+
+public class InMemoryProductData : IProductData
+{
+    public IEnumerable<Section> GetSections() => TestData.Sections;
+  
+    public IEnumerable<Brand> GetBrands() => TestData.Brands;
+   
+
+   
+}
---
+++ WebStore/Sevices/Interfaces/IProductData.cs
@@ -0,0 +1,13 @@
+﻿
+
+using WebStore.Domain.Entites;
+
+namespace WebStore.Sevices.Interfaces;
+
+public interface IProductData
+{   
+    IEnumerable<Section> GetSections();
+    IEnumerable<Brand> GetBrands();
+
+
+}
