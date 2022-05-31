aa7908bb99721aabe81027038698bb71c3f8fac6
DenZone1 <dissap95-d@yandex.ru>
2022-05-31 21:50:58 +0400

    Заготовка котнроллера каталога товаров

--- WebStore/Controllers/CatalogController.cs
+++ WebStore/Controllers/CatalogController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WebStore.Models;
+﻿using WebStore.Domain;
+using WebStore.Sevices.Interfaces;
+using WebStore.ViewModels;
 
 using Microsoft.AspNetCore.Mvc;
 
