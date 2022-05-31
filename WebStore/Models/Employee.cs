3a36f1e1c0b104f1467d60b73411acd34d0ac091
DenZone1 <dissap95-d@yandex.ru>
2022-05-31 19:27:40 +0400

    Валидация в модели представления

--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -4,13 +4,13 @@ public  class Employee
 {
     public int Id { get; set; }
 
-    public string LastName { get; set; }
+    public string LastName { get; set; } = null!;
 
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
 
-    public string Patronymic { get; set; }
+    public string? Patronymic { get; set; }
 
-    public string Age { get; set; }
+    public string Age { get; set; } = null!;
 
     public override string ToString() => $"(id:{Id} {LastName} {Name} {Patronymic} {Age})";
    
--- WebStore/ViewModels/EmployeeViewModel.cs
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -1,17 +1,53 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
+using Microsoft.AspNetCore.Mvc;
+
 namespace WebStore.ViewModels;
 
-public class EmployeeViewModel
+public class EmployeeViewModel : IValidatableObject
 {
+    [HiddenInput]
+    [Display(Name = "идентификатор")]
     public int Id { get; set; }
 
-    public string LastName { get; set; }
 
-    public string Name { get; set; }
+    [Display(Name = "Фамилия")]
+    [Required(ErrorMessage ="Фамилия обязательна")]
+    [StringLength(12, MinimumLength = 2, ErrorMessage ="Длина от 2 до 10")]
+    [RegularExpression("([А-ЯЁ][а-яЁ]+)|([A-Z][a-z]+)", ErrorMessage ="Имя должно начинаться с заглавной буквы")]
+    public string LastName { get; set; } = null!;
 
-    public string Patronymic { get; set; }
+    [Display(Name = "Имя")]
+    [Required(ErrorMessage ="Имя обязательно")]
+    [StringLength(12, MinimumLength = 2, ErrorMessage = "Длина от 2 до 10")]
+    public string Name { get; set; } = null!;
 
-    public string Age { get; set; }
+    [Display(Name = "Отчество")]
+    [StringLength(12, ErrorMessage = "Длина до 12")]
+    public string? Patronymic { get; set; } = null!;
+
+    [Display(Name = "Возраст")]
+    [Range(18,80,ErrorMessage ="от 18 до 80")]
+    public string Age { get; set; } = null!;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (LastName == "Asd" && Name == "Asd" && Patronymic == "Asd")
+            return new[] 
+            { 
+                new ValidationResult("Asd - bad choice", new []
+                {
+                    nameof(LastName), 
+                    nameof(Name),
+                    nameof(Patronymic) 
+                }) 
+            };
+        return new[]
+        {
+            ValidationResult.Success!,
+        };
+    }
 }
 
