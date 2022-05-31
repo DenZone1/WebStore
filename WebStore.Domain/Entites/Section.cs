81d164075508ece43c8baafbea8494a41bbb97dc
DenZone1 <dissap95-d@yandex.ru>
2022-05-31 19:52:39 +0400

    Бренд и Категория(Секция)

---
+++ WebStore.Domain/Entites/Section.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.Domain.Entites.Base.Interfaces;
+using WebStore.Domain.Entites.Base;
+
+namespace WebStore.Domain.Entites;
+
+public class Section : NamedEntity, IOrderedEntity
+{
+    public int Order { get; set; }
+
+    public int? ParentId { get; set; }
+}
