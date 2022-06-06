d4b5d1b5c51b4219cd109bb344acc8c88245a3bd
DenZone1 <dissap95-d@yandex.ru>
2022-06-06 16:29:36 +0400

    Логика и представление системы ркгистрации

--- WebStore.Domain/Entites/Idnetity/User.cs
+++ WebStore.Domain/Entites/Idnetity/User.cs
@@ -6,6 +6,7 @@ namespace WebStore.Domain.Entites.Idnetity;
 
 public class User : IdentityUser
 {
+    public override string ToString() => UserName;
     
 }
 
--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 using WebStore.Domain.Entites.Idnetity;
+using WebStore.ViewModels.Identity;
 
 namespace WebStore.Controllers;
 
@@ -22,7 +23,38 @@ public class AccountController : Controller
         _Logger = Logger;
     }
 
-    public IActionResult Register() => View();
+    public IActionResult Register() => View(new RegisterUserViewModel());
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterUserViewModel Model)
+    {
+        if(!ModelState.IsValid)
+            return View(Model);
+
+        var user = new User 
+        { 
+            UserName = Model.UserName,
+        };
+
+        var creation_result = await _UserManager.CreateAsync(user, Model.Password);
+        if (creation_result.Succeeded)
+        {
+            _Logger.LogInformation("Позователь {0} зарегистрирован", User);
+
+            await _SignInManager.SignInAsync(user,false);
+            return RedirectToAction("Index", "Home");
+        }
+        foreach (var error in creation_result.Errors)
+            ModelState.AddModelError("", error.Description);
+
+        var error_info = string.Join(" , ", creation_result.Errors.Select(e => e.Description));
+        _Logger.LogWarning("Ошибка при регистрации {0}:{1}",user, error_info);
+
+        return View(Model);
+
+
+    }
 
     public IActionResult Login() => View();
 
