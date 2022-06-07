06e7540a4b506cdd12f52de0526f7b8ba6db8186
DenZone1 <dissap95-d@yandex.ru>
2022-06-07 18:18:49 +0400

    Логика и представление системы регистрации

--- WebStore.Domain/Entites/Identity/User.cs
+++ WebStore.Domain/Entites/Identity/User.cs
@@ -5,6 +5,7 @@ namespace WebStore.Domain.Entites.Identity;
 
 public class User : IdentityUser
 {
-
+    public override string ToString() => UserName;
+    
 }
 
---
+++ WebStore/ViewModels/Identity/RegisterUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels.Identity;
+
+public class RegisterUserViewModel
+{
+    [Required(ErrorMessage = "No username specified")]
+    [Display(Name = "User Name")]
+    [MaxLength(255)]
+    public string UserName { get; set; } = null!;
+
+    [Required(ErrorMessage = "The password is required")]
+    [Display(Name = "Password")]
+    [DataType(DataType.Password)]
+    [MaxLength(255)]
+    public string Password { get; set; } = null!;
+
+    [Required(ErrorMessage = "Password confirmation not entered")]
+    [Display(Name = "Confirm password")]
+    [DataType(DataType.Password)]
+    [MaxLength(255)]
+    [Compare(nameof(Password), ErrorMessage = "Password and confirmation do not match")]
+    public string PasswordConfirm { get; set; } = null!;
+}
