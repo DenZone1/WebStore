ad1de02a5e63da4273a466ee4dfd58a10a6ea5d4
DenZone1 <dissap95-d@yandex.ru>
2022-06-08 16:23:28 +0400

    Заготовка контроллера Аккаунта

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Domain.Entites.Identity;
+
+namespace WebStore.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly UserManager<User> _UserMAnager;
+    private readonly SignInManager<User> _SignInManager;
+    private readonly ILogger<AccountController> _Logger;
+
+    public AccountController(
+        UserManager<User> UserMAnager,
+        SignInManager<User> SignInManager,
+        ILogger<AccountController> Logger)
+    {
+        _UserMAnager = UserMAnager;
+        _SignInManager = SignInManager;
+        _Logger = Logger;
+    }
+
+    public IActionResult Register() => View();
+
+    public IActionResult Login() => View();
+
+    public IActionResult Logout() => View();
+
+    public IActionResult AccesDenied() => View();
+}
