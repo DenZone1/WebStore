f79a56061768d61c5c3dac5249d3fec2906d53fa
DenZone1 <dissap95-d@yandex.ru>
2022-06-08 19:41:04 +0400

    Постраничное разбиение сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -19,9 +19,22 @@ public class EmployeesController : Controller
         _Mapper = Mapper;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(int? Page, int PageSize = 15)
     {
-        var employees = _Employees.Getall();
+        //var employees = _Employees.Getall();
+
+        IEnumerable<Employee> employees;
+        if (Page is { } page && PageSize > 0)
+        {
+            employees = _Employees.Get(page * PageSize, PageSize);
+        }
+        else
+        employees = _Employees.Getall();
+
+        ViewBag.PagesCount = PageSize > 0 ?
+            (int?)Math.Ceiling(_Employees.GetCount() / (double)PageSize)
+            : null!;
+
         return View(employees);
 
     }
