7d537c384423f8d38c094645c15c92f3385e06be
DenZone1 <dissap95-d@yandex.ru>
2022-06-08 16:15:00 +0400

    Конфигурация системы Identity

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,16 +1,58 @@
-using WebStore.Imfrastructure.Middleware;
-using WebStore.Imfrastructure.Conventions;
-using WebStore.Sevices.Interfaces;
-using WebStore.DAL.Context;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+
+using WebStore.DAL.Context;
 using WebStore.Data;
+using WebStore.Domain.Entites.Identity;
+using WebStore.Imfrastructure.Conventions;
+using WebStore.Imfrastructure.Middleware;
 using WebStore.Sevices.InMemory;
 using WebStore.Sevices.InSQL;
+using WebStore.Sevices.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 //конфигурирование состаных частей приложения
 
 var services = builder.Services;
+
+services.AddIdentity<User, Role>()
+    .AddEntityFrameworkStores<WebStoreDB>()
+    .AddDefaultTokenProviders();
+
+services.Configure<IdentityOptions>(opt => 
+{
+#if DEBUG
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequiredLength = 3;
+    opt.Password.RequiredUniqueChars = 3;
+#endif
+
+    opt.User.RequireUniqueEmail = false;
+    opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ1234567890";
+
+    opt.Lockout.AllowedForNewUsers = false;
+    opt.Lockout.MaxFailedAccessAttempts = 10;
+    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+
+});
+
+services.ConfigureApplicationCookie(opt =>
+{
+    opt.Cookie.Name = "WebStore";
+    opt.Cookie.HttpOnly = true;
+
+    opt.ExpireTimeSpan = TimeSpan.FromDays(10);
+
+    opt.LoginPath = "/Account/Login";
+    opt.LogoutPath = "/Account/Logout";
+    opt.AccessDeniedPath = "/Account/AccessDenied";
+
+    opt.SlidingExpiration = true;
+});
+
 services.AddScoped<IEmployeesData, InMemoryEmployeesData>();//контейнер сервисов(самый универсальный)
 services.AddScoped<IProductData, SqlProductData>();
 
