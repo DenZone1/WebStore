68688440c284e199f0889e07fb433c36612431fa
DenZone1 <dissap95-d@yandex.ru>
2022-06-11 18:26:09 +0400

    Пароль

--- WebStore.Domain/Entites/Identity/User.cs
+++ WebStore.Domain/Entites/Identity/User.cs
@@ -5,7 +5,7 @@ namespace WebStore.Domain.Entites.Identity;
 public class User : IdentityUser
 {
     public const string Administrator = "Admin";
-    public const string AdminPassword = "AdPass_000999";
+    public const string AdminPassword = "123456";
     public override string ToString() => UserName;
     
 }
