e83abebeae1a8d85ccf5bb9a1dc637d011799754
DenZone1 <dissap95-d@yandex.ru>
2022-06-11 19:30:35 +0400

    Заготовка котроллер управления товарми в каталоге

--- WebStore.Domain/Entites/Identity/User.cs
+++ WebStore.Domain/Entites/Identity/User.cs
@@ -5,7 +5,7 @@ namespace WebStore.Domain.Entites.Identity;
 public class User : IdentityUser
 {
     public const string Administrator = "Admin";
-    public const string AdminPassword = "123456";
+    public const string AdminPassword = "AdPAss_123";
     public override string ToString() => UserName;
     
 }
---
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Domain.Entites.Identity;
+
+namespace WebStore.Areas.Admin.Controllers;
+
+[Authorize(Roles = Role.Administrator)]
+public class ProductsController : Controller
+{
+    public IActionResult Index() => View();
+}
