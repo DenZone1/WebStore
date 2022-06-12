cbf3624becd9726e91b26c85415f3215e2fa693c
DenZone1 <dissap95-d@yandex.ru>
2022-06-12 18:38:49 +0400

    Отображение списка товаров

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -2,11 +2,24 @@
 using Microsoft.AspNetCore.Mvc;
 
 using WebStore.Domain.Entites.Identity;
+using WebStore.Sevices.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers;
 
 [Authorize(Roles = Role.Administrator)]
 public class ProductsController : Controller
 {
-    public IActionResult Index() => View();
+    private readonly IProductData _ProductData;
+    private readonly ILogger<ProductsController> _Logger;
+
+    public ProductsController(IProductData ProductData, ILogger<ProductsController> Logger)
+    {
+        _ProductData = ProductData;
+        _Logger = Logger;
+    }
+    public IActionResult Index()
+    {
+        var products = _ProductData.GetProducts();
+        return View(products);
+    }
 }
